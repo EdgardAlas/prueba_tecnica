edda1ad427581444d26aa7da9b93b3d412dd548e
Edgard Alas <edgardalasu@gmail.com>
2023-09-14 16:05:41 -0600

    bugfix: removing .env

--- fronted/src/config/config.ts
+++ fronted/src/config/config.ts
@@ -1,5 +1,5 @@
 export const config = {
-  apiUrl: import.meta.env.VITE_APP_API_URL,
+  apiUrl: import.meta.env.VITE_APP_API_URL || 'http://localhost:8000/api',
   oficial: 2,
   residental: 1,
   noOficial: 3,
