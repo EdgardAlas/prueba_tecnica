f5066e30f8c9c8c72f0cd49a664bc7ca174c6320
Edgard Alas <edgardalasu@gmail.com>
2023-09-14 23:35:11 -0600

    feat: docker compose added

--- fronted/src/config/config.ts
+++ fronted/src/config/config.ts
@@ -1,5 +1,5 @@
 export const config = {
-  apiUrl: import.meta.env.VITE_APP_API_URL || 'http://localhost:8000/api',
+  apiUrl: import.meta.env.VITE_APP_API_URL || 'http://localhost/api',
   oficial: 2,
   residental: 1,
   noOficial: 3,
